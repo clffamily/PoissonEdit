19c2e8060f130c96d2200bf4bf07da997a74e958
Lingfeng Cai <georgelfcai@gmail.com>
2016-01-03 16:49:52 +0800

    modify the cut script.

--- cut/data.m
+++ cut/data.m
@@ -2,12 +2,14 @@ clear all;
 load CurrRes.mat;
 
 imwrite(uint8(CurrRes),'images/object.jpg');
-imwrite((CurrRes),'images/object_mask.jpg');
 
 maskMatrix = CurrRes(:, :, 1);
 maskMatrix(maskMatrix ~= 0) = -1;
 
 user_mask = ~fixedBG;
-imwrite(user_mask,'images/user_mask.jpg');
+imwrite(uint8(user_mask),'images/user_mask.jpg');
+
+load imBounds.mat;
+imwrite(uint8(imBounds),'images/user_draw.jpg');
 
 maskMatrix(user_mask == 0) = -1;
