3492d5f53777e5d7dc706cab799316d9e91d1aac
Lingfeng Cai <georgelfcai@gmail.com>
2016-01-11 00:23:01 +0800

    lab automation script

---
+++ path/labScript.m
@@ -0,0 +1,34 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These are parameters to specify.
+simg = double(imread('../images/apple/apple.jpg', 'JPG'));
+timg = double(imread('../images/apple/table.jpg', 'JPG'));
+xPara = 750;
+yPara = 240;
+maxItr = 5;
+
+% Remember to specify the paramters in the script.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+countItr = 1;
+
+% First para is row(Y)
+previousK = calAvgKLAB( simg, timg, boundaryPts, yPara, xPara); 
+
+% First para is row(Y), maskMatrix is from data.m
+costMatrix = calCostMatrixLAB( simg, timg, maskMatrix, yPara, xPara, previousK); 
+
+% Find the shortest path in RGB graph path.
+findShortestPath; % Remember to specify the paramters in the script.
+
+for i = 1 : maxItr
+    currentK = calAvgKLAB( simg, timg, mat, yPara, xPara); 
+    if previousK == currentK
+        break
+    end
+    costMatrix = calCostMatrixLAB( simg, timg, maskMatrix, yPara, xPara, currentK);
+    previousK = currentK;
+    findShortestPath;
+    countItr = countItr + 1;
+end
+
+disp(sprintf('Totally running rgb interations for %g times.', countItr));
