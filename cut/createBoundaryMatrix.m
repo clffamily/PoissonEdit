a097636e5a75e1e1a31e48f9d1ed104bc5069af0
Lingfeng Cai <georgelfcai@gmail.com>
2016-01-10 16:15:36 +0800

    create boundary for RGB image
    Drag and drop.

---
+++ cut/createBoundaryMatrix.m
@@ -0,0 +1,11 @@
+boundary = bwboundaries(user_mask);
+
+boundaryPts = zeros(size(user_mask));
+
+for i = 1 : size(boundary{1},1)
+    firstNum = boundary{1}(i,1);
+    secondNum = boundary{1}(i,2);
+    boundaryPts(firstNum, secondNum) = 1;
+end 
+
+imshow(boundaryPts);
